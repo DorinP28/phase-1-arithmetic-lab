c7bcc020a2f8d11067b83a9cb83c39f858094084
Dorin Porcereanu <porcereanu.dorin@gmail.com>
2023-03-16 14:21:36 -0400

    done

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+console.log(multiply)
 
-// Write your code here
+let random = Math.ceil(Math.random() * 100);
+
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4; // calculates the remainder of dividing num3 by num4, such that the remainder is 4
+console.log(mod);
+
+let num5 = 4
+let num6 = 8
+let num7 = 12
+let num8 = 16
+let num9 = 20
+let max = Math.max(num5, num6, num7, num8, num9)
+console.log(max)
